68b2a2e24089cb4d77d5a141e4881d439ccc3971
Akash Srivastava <akashniki@gmail.com>
2019-10-30 21:22:29 +0530

    sm8150-common: Enable perfconfigstore

--- sm8150.mk
+++ sm8150.mk
@@ -264,6 +264,10 @@ PRODUCT_COPY_FILES += \
 #    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
 #    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
