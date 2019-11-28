a9fe3bf617001f0940c1c2110b75e9b0951e7b0a
Demon000 <demonsingur@gmail.com>
2019-11-28 02:34:41 +0100

    sm8150-common: build QTI value add check package
    Change-Id: If5ae3d5d00ddd6a601b238e9968ed4ca11b71db6

--- sm8150.mk
+++ sm8150.mk
@@ -300,6 +300,9 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 # QTI
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml
