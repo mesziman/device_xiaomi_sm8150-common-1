c9370247970826102da969f62e5e4ba92cb19566
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-02-06 12:45:04 +0000

    remove duplicate

--- sm8150.mk
+++ sm8150.mk
@@ -320,11 +320,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# RIL
-PRODUCT_PACKAGES += \
-    libjson \
-    libril \
-    librilutils
 
 # Recovery
 PRODUCT_PACKAGES += \
