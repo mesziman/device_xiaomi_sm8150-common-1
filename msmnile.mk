ec1ca3c582fc4be126ba855dbe4940935f5f6576
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-03-23 11:50:37 +0100

    Revert "syberia-fy"
    This reverts commit 25595164ab52a0c0b1da536ee7fbd08f6f1f8697.

--- msmnile.mk
+++ msmnile.mk
@@ -43,7 +43,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-syb
+    $(LOCAL_PATH)/overlay-pe
 
 # Properties
 -include $(LOCAL_PATH)/device_props.mk
