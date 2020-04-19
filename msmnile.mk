decad97271bfc8d193bf074a1606720f68f489a8
Demon000 <demonsingur@gmail.com>
2020-04-19 13:09:43 +0200

    fix pin

--- msmnile.mk
+++ msmnile.mk
@@ -49,7 +49,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-pe 
+
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
