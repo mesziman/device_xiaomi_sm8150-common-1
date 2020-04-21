f630b894d7d541c1a27eb96ecdc68e70338db010
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-04-21 17:52:39 +0200

    Revert "    sm6150-common: power-libperfmgr: Scan input devices for eligible DT2W node"
    This reverts commit 24e393ad53b03208bdc6d82e7865bda1000aa661.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=16 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+##BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -166,8 +167,7 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 1610612736
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
@@ -177,9 +177,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_TAP_TO_WAKE_EVENT_NODE := "/dev/input/event4"
-
-TARGET_TAP_TO_WAKE_NODE := "/dev/input/event4"
-
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_USES_NON_LEGACY_POWERHAL := true
 
@@ -238,7 +235,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 QC_WIFI_HIDL_FEATURE_DUAL_AP := true
 
 #TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-
+TARGET_FLATTEN_APEX := true
 #PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION += $(shell find $(DEVICE_PATH)/jar-profiles/ -iname '*.txt')
 # Inherit from the proprietary version
 -include vendor/xiaomi/cepheus/BoardConfigVendor.mk
