c16b2f057188d62ca758947c3490bbe2efa057a4
Akash Srivastava <akashniki@gmail.com>
2020-04-23 09:39:49 +0200

    cepheus:overlay: Add multiuser support
    Conflicts:
    	overlay/frameworks/base/core/res/res/values/config.xml

--- msmnile.mk
+++ msmnile.mk
@@ -132,6 +132,16 @@ PRODUCT_PACKAGES += \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
+PRODUCT_PACKAGES += libbluetooth_qti
+PRODUCT_PACKAGES += bt_logger
+PRODUCT_PACKAGES += libbt-logClient
+PRODUCT_PACKAGES += BluetoothExt
+PRODUCT_PACKAGES += libbtconfigstore
+PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@1.0
+PRODUCT_PACKAGES += com.qualcomm.qti.bluetooth_audio@1.0
+PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_audio@2.0
+PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
