994b99bb7a2b6496d4b155d9718d04d2e3de1752
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-04-24 18:32:06 +0200

    revert nfc

--- msmnile.mk
+++ msmnile.mk
@@ -346,23 +346,18 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     com.gsma.services.nfc \
     com.nxp.nfc.nq \
-    NQNfcNci \
+    NfcNci \
     Tag \
-    libnqnfc_nci_jni \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
 
 PRODUCT_BOOT_JARS += com.nxp.nfc.nq
 
 PRODUCT_PACKAGES += \
-    SecureElement \
     nfc_nci.nqx.default.hw \
     vendor.nxp.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
