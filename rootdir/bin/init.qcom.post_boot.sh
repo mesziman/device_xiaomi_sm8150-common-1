55070d3068a483ab9dcc3a2a9c7afdb0ca055829
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-05-09 23:35:30 +0200

    Revert "raphael: Add fuction to configurate UX task cgroups to post_boost.sh"
    This reverts commit f7e0f2f234d0a84e655a9f9052126087c30144cf.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -95,48 +95,6 @@ function configure_memory_parameters() {
     configure_read_ahead_kb_values
 }
 
-function configure_ux_task_cgroup_membership() {
-   # EXPERIMENTAL: Optimize UX task cgroup membership
-	PIDSS=`ps -AT | grep system_server | awk '{print $3}'`
-
-	echo $PIDSS > /dev/cpuset/foreground/cgroup.procs
-	echo $PIDSS > /dev/stune/foreground/cgroup.procs
-
-	PIDAIO=`ps -AT | grep android.io | awk '{print $3}'`
-
-	echo $PIDAIO > /dev/stune/foreground/tasks
-
-	PIDAA=`ps -AT | grep android.anim | awk '{print $3}'`
-
-	echo $PIDAA > /dev/cpuset/top-app/tasks
-
-	PIDAALF=`ps -AT | grep android.anim.lf | awk '{print $3}'`
-
-	echo $PIDAALF > /dev/cpuset/top-app/tasks
-
-	PIDAFG=`ps -AT | grep android.fg | awk '{print $3}'`
-
-	echo $PIDAFG > /dev/stune/foreground/tasks
-
-	PIDAUI=`ps -AT | grep android.ui | awk '{print $3}'`
-
-	echo $PIDAUI > /dev/stune/top-app/tasks
-
-	PIDAD=`ps -AT | grep android.display | awk '{print $3}'`
-
-	echo $PIDAD > /dev/cpuset/top-app/tasks
-	echo $PIDAD > /dev/stune/top-app/tasks
-
-	PIDAS=`ps -AT | grep ndroid.systemui | awk '{print $3}'`
-
-	echo $PIDAS > /dev/cpuset/top-app/tasks
-	echo $PIDAS > /dev/stune/top-app/tasks
-
-	PIDRECLAIMD=`ps -AT | grep reclaimd | awk '{print $3}'`
-
-	echo $PIDRECLAIMD > /dev/stune/top-app/tasks
-
-}
 case "$target" in
     "msmnile")
     # Core control parameters for gold
@@ -270,8 +228,6 @@ case "$target" in
     configure_memory_parameters
 
     echo "18432,23040,27648,38708,102356,144768" > /sys/module/lowmemorykiller/parameters/minfree
-    configure_ux_task_cgroup_membership
-
     ;;
 esac
 
