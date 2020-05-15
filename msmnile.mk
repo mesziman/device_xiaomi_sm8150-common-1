616989cdd7b5e5e97632674484c1bd1a188fac21
Gabor <meszaros.gabor.10k@gmail.com>
2020-05-15 09:58:09 +0200

    cepheus: build libqdMetaData.system

--- msmnile.mk
+++ msmnile.mk
@@ -512,7 +512,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libdisplayconfig \
     libdisplayconfig.vendor \
-    libqdMetaData 
+    libqdMetaData \
+    libqdMetaData.system
 
 PRODUCT_BOOT_JARS += tcmiface
 
